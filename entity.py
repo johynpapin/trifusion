02ad6b394e3dadb2fde2d4b3e6b5e3c339fe607e
M3RCHU <merchu@LAPTOP-HB8HPN25>
2019-04-27 07:35:34 +0200

    deux trucs

--- entity.py
+++ entity.py
@@ -7,5 +7,17 @@ class Entity:
         self.position = position
 
 class SlimeEntity(Entity):
-    def __init__(self, sprite, enchantment, speed):
-        super(sprite, enchantment, speed)
+    def __init__(self, sprite, enchantment, speed, mind, item, life_spawn, special):
+        super(sprite, enchantment, speed, mind, item, life_spawn, special)
+        mind = 5
+        item = 1
+        life_spawn = 100
+        special = None
+
+class GoblinEntity(Entity):
+    def __init__(self, sprite, enchantment, speed, mind, item, life_spawn, special):
+        super(sprite, enchantment, speed, mind, item, life_spawn, special)
+        mind = 7
+        item = 5
+        life_spawn = 100
+        special = Launchable
