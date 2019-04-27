4410111e5117dea35217da08cd32c0d9175e084a
Papin Johyn <johynpapin@protonmail.com>
2019-04-27 11:11:39 +0200

    olala

--- game/grid.py
+++ game/grid.py
@@ -1,34 +1,37 @@
 import pyglet
-from . import utils
+from .utils import Position
 from . import resources
 
 class Grid:
     def __init__(self):
         self.grid = {}
-        self.camera = utils.Position(0, 0)
+        self.camera = Position(0, 0)
+        self.sprites = []
 
     def is_empty(self, position):
         return position not in self.grid
 
     def get_tile(self, position):
         if self.is_empty(position):
-            self.grid[position] = Tile()
+            self.grid[position] = Tile(position)
 
         return self.grid[position]
 
-    def get_batch(self, offset):
-        batch = pyglet.graphics.Batch()
-        
+    def draw(self, batch, offset):
         for x in range(10):
             for y in range (10):
-                if self.is_empty(utils.Position(x, y)):
-                    sprite = pyglet.sprite.Sprite(img=resources.grass_image, batch=batch, x=offset.x + x * 70, y=offset.y + y * 70)
-
-        return batch
+                tile = self.get_tile(Position(x, y))
+                tile.draw(batch, offset)
 
 class Tile:
-    def __init__():
+    def __init__(self, position):
         self.resource = None
+        self.position = position
 
     def has_resource(self):
         return self.resource is not None
+
+    def draw(self, batch, offset):
+        self.sprite = pyglet.sprite.Sprite(img=resources.grass_image, batch=batch)
+        self.sprite.x = self.position.x * 70 + offset.x
+        self.sprite.y = self.position.y * 70 + offset.y
--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from game.grid import Grid
 from game.utils import Position
 from game.spell import MoveSpell, HarvestSpell
 from game.enchantment import SimpleEnchantment
+import game.resources as resources
 
 window = pyglet.window.Window(visible=False)
 window.set_caption('Legend Of Wizard')
@@ -24,9 +25,12 @@ spells = [MoveSpell, HarvestSpell]
 @window.event
 def on_draw():
     window.clear()
-
-    batch = grid.get_batch(Position(0, 0))
-    batch.draw()
+    
+    main_batch = pyglet.graphics.Batch()
+    
+    grid.draw(main_batch, Position(0, 0))
+    
+    main_batch.draw()
 
 def update(dt):
     for entity in entities:
